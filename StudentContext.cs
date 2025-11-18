3856d5c4f333b71c64cf361b601a1fe169420851
ZeinabH17 <zeinabhussein105@gmail.com>
2025-11-18 16:25:36 -0500

    Create StudentContext.cs

---
+++ StudentContext.cs
@@ -0,0 +1,11 @@
+using System.Data.Entity;
+
+namespace StudentCodeFirst
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("StudentDB")  // Connection name
+        
+
+        public DbSet<Student> Students { get; set; }
+    
